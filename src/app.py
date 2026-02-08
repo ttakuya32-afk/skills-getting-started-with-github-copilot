01a96d4fd3971ff1073a6ccdc1918053e6607a6c
ttakuya32-afk <t.takuya32@gmail.com>
2026-02-08 13:47:50 +0000

    of the Chess Club activity entry in the in-memory database

--- src/app.py
+++ src/app.py
@@ -22,7 +22,7 @@ app.mount("/static", StaticFiles(directory=os.path.join(Path(__file__).parent,
 # In-memory activity database
 activities = {
     {
-        "Chess Club": {
+        "Chess Club": { 
             "description": "Learn strategies and compete in chess tournaments",
             "schedule": "Fridays, 3:30 PM - 5:00 PM",
             "max_participants": 12,
